d5458fdf399be81714dab884efb70e1aa4c1dee8
Edu58 <edumuriithi58@gmail.com>
2021-12-11 10:04:58 +0300

    responsiveness

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const API_KEY = "AIzaSyCRIBLv287ZWf90Wq4HM3GUAHZQmEHOUqI";
 const BASE_URL = " https://www.googleapis.com/youtube/v3/videos?";
-const NUM_1 = document.getElementById("home-banner");
 const NUM_1_TITLE = document.getElementById("video-title");
 const DESCRIPTION = document.getElementById("movie-description");
 const POSTERS = document.getElementById("thumbnail-rows");
 const PLAY = document.getElementById("play");
 const views = document.getElementById("views");
+const bannerBG = document.getElementById("banner-bg");
 
 const params = new URLSearchParams({
   key: API_KEY,
@@ -15,22 +15,11 @@ const params = new URLSearchParams({
   regionCode: "KE",
 });
 
-const getViewsParams = new URLSearchParams({
-  key: API_KEY,
-  part: "statistics",
-  chart: "mostPopular",
-  maxResults: 20,
-  regionCode: "KE",
-});
-
 fetch(BASE_URL + params)
   .then((response) => response.json())
   .then((data) => {
     NUM_1_TITLE.innerText = data.items[0].snippet.title;
-    NUM_1.style.background = `url('${data.items[0].snippet.thumbnails.maxres.url}')`;
-    NUM_1.style.backgroundRepeat = "no-repeat";
-    NUM_1.style.backgroundSize = "cover";
-    NUM_1.style.backgroundPosition = "center";
+    bannerBG.src = data.items[0].snippet.thumbnails.maxres.url;
 
     PLAY.addEventListener("click", () => {
       console.log(data.items[0].id);
@@ -42,24 +31,22 @@ fetch(BASE_URL + params)
 
     sliced.forEach((item) => {
       POSTERS.src = item.snippet.thumbnails.medium.url;
-      //console.log(item.snippet.description)
-
+      
       const YTBASE_URL = "https://www.youtube.com/watch?v=";
-
-      html += `
+              
+              html += `
       <div class="video">
             <img src="${item.snippet.thumbnails.high.url}" alt="" />
             <div class="details">
               <h2>${item.snippet.title}</h2>
               <p>${item.snippet.description.slice(0, 300)}....</p>
-              <button class="play"><a href=${
-                YTBASE_URL + item.id
-              } target="_blank">Play</a></button>
+              <button class="play"><a href=${YTBASE_URL + item.id
+                } target="_blank">Play</a></button>
             </div>
           </div>
                 `;
-    });
-    POSTERS.innerHTML = html;
+        });
+      POSTERS.innerHTML = html;
   });
 
 fetch(BASE_URL + getViewsParams)
@@ -68,5 +55,5 @@ fetch(BASE_URL + getViewsParams)
     let good_num = data.items[0].statistics.viewCount
     let gooood_num = parseInt(good_num) //because the value received is a string
     let perfect_num = gooood_num.toLocaleString() //adds commas
-    views.innerText = `Views: ${perfect_num}`;
+    views.innerText = `Views:  ${perfect_num}`;
   });
